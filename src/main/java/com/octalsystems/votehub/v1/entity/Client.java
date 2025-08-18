67cb151058b60714187401b076c93989ad20cd6c
catharyna-angela <catharyna.angela.1@outlook.com>
2025-08-18 20:10:40 -0300

    feature: adicionando entidade client e roles

---
+++ src/main/java/com/octalsystems/votehub/v1/entity/Client.java
@@ -0,0 +1,49 @@
+package com.octalsystems.votehub.v1.entity;
+
+import com.octalsystems.votehub.v1.utils.Roles;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "clientes")
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "senha", nullable = false)
+    private String password;
+
+    @Column(name = "e-mail", nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "perfil", nullable = false)
+    private Roles rolesEnum = Roles.ROLE_CLIENT;
+
+    @Column(name = "validado", nullable = false)
+    private boolean validated = false;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/octalsystems/votehub/v1/utils/Roles.java
@@ -0,0 +1,5 @@
+package com.octalsystems.votehub.v1.utils;
+
+public enum Roles {
+    ROLE_USER, ROLE_ADMIN, ROLE_CLIENT;
+}
