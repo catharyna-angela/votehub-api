bf08a6a6d492c718afc086bebe4d2c92cea4a5a5
catharyna-angela <catharyna.angela.1@outlook.com>
2025-08-18 16:32:33 -0300

    chore: adicionando configs, pacotes e libs

---
+++ src/main/java/com/octalsystems/votehub/VotehubApiApplication.java
@@ -0,0 +1,13 @@
+package com.octalsystems.votehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotehubApiApplication.class, args);
+	}
+
+}
