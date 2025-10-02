28af74d69fbf10739e29e5e9c286e2beda924c61
catharyna-angela <catharyna.angela.1@outlook.com>
2025-10-01 22:33:06 -0300

    chore: ajustando código

--- src/main/java/com/octalsystems/votehub/v1/jwt/JwtService.java
+++ src/main/java/com/octalsystems/votehub/v1/jwt/JwtService.java
@@ -4,9 +4,6 @@ import com.octalsystems.votehub.v1.dto.LoginResponseDTO;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.MacAlgorithm;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,18 +14,13 @@ import java.util.Date;
 
 @Component
 @Slf4j
-@Setter
-@Getter
-@AllArgsConstructor
 public class JwtService {
-    private static final String JWT_BEARER = "Bearer ";
-    private static final String JWT_AUTHORIZATION = "Authorization";
     private static final long EXPIRATION_MILLIS = 240000L;
     private static final MacAlgorithm signatureAlgorithm = Jwts.SIG.HS256;
     private final SecretKey secretKey;
 
-    public JwtService(@Value("${jwt.secret}") String secretKey) { //construtor
-        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
     public LoginResponseDTO generateToken(String email, String role, String id, String issuer) {
@@ -36,7 +28,7 @@ public class JwtService {
         Date limit = new Date(now.getTime() + EXPIRATION_MILLIS);
 
         String token = Jwts.builder()
-                .header().add("typ","JWT").and()
+                .header().add("typ", "JWT").and()
                 .id(id)
                 .issuedAt(now)
                 .expiration(limit)
