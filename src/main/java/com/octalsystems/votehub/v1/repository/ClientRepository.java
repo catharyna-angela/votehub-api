9b0833e8e9bde865d188ac92c74c967f782bb70c
catharyna-angela <catharyna.angela.1@outlook.com>
2025-10-01 22:50:12 -0300

    feat: adicionando busca por email no JPA

--- src/main/java/com/octalsystems/votehub/v1/repository/ClientRepository.java
+++ src/main/java/com/octalsystems/votehub/v1/repository/ClientRepository.java
@@ -3,6 +3,8 @@ package com.octalsystems.votehub.v1.repository;
 import com.octalsystems.votehub.v1.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {
+import java.util.Optional;
 
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByEmail(String email);
 }
