7202959e3561dad0efc45533eddf8cfa1957f3b3
catharyna-angela <catharyna.angela.1@outlook.com>
2025-10-01 22:38:06 -0300

    feat: implementando autenticação e autorização

---
+++ src/main/java/com/octalsystems/votehub/v1/jwt/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.octalsystems.votehub.v1.jwt;
+
+import com.octalsystems.votehub.v1.entity.Client;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final Client client;
+
+    public Long getId() {
+        return client.getId();
+    }
+
+    public String getRole() {
+        return client.getRole().name();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(client.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return client.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return client.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/octalsystems/votehub/v1/service/AuthService.java
+++ src/main/java/com/octalsystems/votehub/v1/service/AuthService.java
@@ -3,9 +3,12 @@ package com.octalsystems.votehub.v1.service;
 import com.octalsystems.votehub.v1.dto.LoginDTO;
 import com.octalsystems.votehub.v1.dto.LoginResponseDTO;
 import com.octalsystems.votehub.v1.jwt.JwtService;
-import jakarta.validation.Valid;
+import com.octalsystems.votehub.v1.jwt.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +17,28 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-    public LoginResponseDTO authenticate(@Valid LoginDTO loginDTO) {
+    public LoginResponseDTO authenticate(LoginDTO loginDTO) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginDTO.getEmail(), //principal
+                            loginDTO.getPassword() //credentials
+                    )
+            );
 
-        return null;
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+            return jwtService.generateToken(
+                    userDetails.getUsername(),
+                    userDetails.getRole(),
+                    String.valueOf(userDetails.getId()),
+                    "/api/v1/auth/login"
+            );
+
+        } catch (Exception ex) {
+            throw new RuntimeException("'Credenciais inválidas'", ex);
+        }
     }
 }
