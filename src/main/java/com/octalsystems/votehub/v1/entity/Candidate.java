28d3d2a1e803f2a3a973ff6348cd986fd68389e3
catharyna-angela <catharyna.angela.1@outlook.com>
2025-11-15 13:46:18 -0300

    feat: criando entidade candidatos

---
+++ src/main/java/com/octalsystems/votehub/v1/entity/Candidate.java
@@ -0,0 +1,36 @@
+package com.octalsystems.votehub.v1.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "candidatos")
+public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    private boolean vote = false; //vai receber "true" quando for votado e em seguida incrementado em "totalVotes".
+
+    //fixme: criar atributo "version".
+
+    @Column(name = "total_de_votos")
+    private int totalVotes = 0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voting_id")
+    private Voting voting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "poll_id")
+    private Poll poll;
+
+}
