f864d3016be8967cf181707d06386fac81a72b0d
catharyna-angela <catharyna.angela.1@outlook.com>
2025-12-24 00:13:23 -0300

    refactor: adição de logs, ordem e tipo de método

--- src/main/java/com/octal/votehub/api/v1/util/CodeService.java
+++ src/main/java/com/octal/votehub/api/v1/util/CodeService.java
@@ -3,12 +3,14 @@ package com.octal.votehub.api.v1.util;
 import com.octal.votehub.api.v1.entity.Code;
 import com.octal.votehub.api.v1.repository.CodeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CodeService {
@@ -18,7 +20,7 @@ public class CodeService {
 
     private final CodeRepository codeRepository;
 
-    private String generate() {
+    private static String generate() {
         StringBuilder sb = new StringBuilder(6);
 
         for (int i = 0; i < 6; i++) {
@@ -26,6 +28,16 @@ public class CodeService {
         }
 
         return sb.toString();
+    }
+
+    private void isExpired(Code code){
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        LocalDateTime expiration = code.getExpirationDate();
+
+        if (dateTimeNow.isAfter(expiration)) {
+            log.info("'Código para ativação de conta expirado.'");
+            throw new RuntimeException("'Código expirado.'");
+        }
     }
 
     @Transactional
@@ -44,19 +56,12 @@ public class CodeService {
     @Transactional(readOnly = true)
     public void validate(String code){
         Code codeIsPresent = codeRepository.findByCode(code)
-                .orElseThrow(() -> new RuntimeException("'O código recebido não está correto.'"));
+                .orElseThrow(() -> {
+                    log.info("'O código de ativação de conta não confere.'");
+                    return new RuntimeException("'O código recebido não está correto.'");
+                });
 
         isExpired(codeIsPresent);
-
-    }
-
-    private void isExpired(Code code){
-        LocalDateTime dateTimeNow = LocalDateTime.now();
-        LocalDateTime expiration = code.getExpirationDate();
-
-        if (dateTimeNow.isAfter(expiration)) {
-            throw new RuntimeException("'Código expirado.'");
-        }
     }
 
 }
