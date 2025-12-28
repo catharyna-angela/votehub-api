1839ea4d56409df1ab5334c066c9ed3627ba91ae
catharyna-angela <catharyna.angela.1@outlook.com>
2025-12-28 00:13:43 -0300

    feat: adicionando mapper para candidate

---
+++ src/main/java/com/octal/votehub/api/v1/dto/mapper/CandidateMapper.java
@@ -0,0 +1,18 @@
+package com.octal.votehub.api.v1.dto.mapper;
+
+import com.octal.votehub.api.v1.dto.candidate.ChooseCandidateDTO;
+import com.octal.votehub.api.v1.dto.candidate.ResponseCandidateDTO;
+import com.octal.votehub.api.v1.entity.Candidate;
+import org.modelmapper.ModelMapper;
+
+public class CandidateMapper {
+
+    public static Candidate toCandidate(ChooseCandidateDTO chooseCandidateDTO) {
+        return new ModelMapper().map(chooseCandidateDTO, Candidate.class);
+    }
+
+    public static ResponseCandidateDTO toResponseCandidateDTO(Candidate candidate){
+        return new ModelMapper().map(candidate, ResponseCandidateDTO.class);
+    }
+
+}
