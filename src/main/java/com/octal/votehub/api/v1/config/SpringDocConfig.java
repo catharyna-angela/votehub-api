4df9647cde67a844f0b6b2dbf5aa87f6f52a1d2e
catharyna-angela <catharyna.angela.1@outlook.com>
2026-01-04 14:10:25 -0300

    fix: ajustando configuração do springdoc

--- src/main/java/com/octal/votehub/api/v1/config/SpringDocConfig.java
+++ src/main/java/com/octal/votehub/api/v1/config/SpringDocConfig.java
@@ -1,11 +1,12 @@
 package com.octal.votehub.api.v1.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SpringDocConfig {
     @Bean
     public OpenAPI openAPI() {
@@ -15,7 +16,6 @@ public class SpringDocConfig {
                         .description("API REST para criação de votação e enquetes.")
                         .version("v1")
                         .license(new License().name("AGPL-3.0").url("https://www.gnu.org/licenses/agpl-3.0.html"))
-                        .contact(new Contact().url("https://www.linkedin.com/in/catharynaas"))
         );
     }
 }
